5e0c17211bb9e7ecf1b4a1c0a456081010826133
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2024-10-13 10:37:17 +0700

    Conditional Expression If-Else

---
+++ app/src/main/java/com/example/muzaka1project/ConditionalExpression.kt
@@ -0,0 +1,21 @@
+package com.example.muzaka1project
+
+// Conditional Expression
+// Conditional Expression adalah ekspresi yang mengembalikan nilai berdasarkan kondisi tertentu
+// Conditional Expression menggunakan if-else, when, dan try-catch
+
+class ConditionalExpression {
+    fun ConditionalStatement(){
+        println("=== Conditional Expression ===")
+
+        // If-Else
+        val number = 10
+        if (number > 0){
+            println("Positive Number")
+        } else {
+            println("Negative Number")
+        }
+
+
+    }
+}
