0bf1927076d6206c9f1d5900a7a3a00a3fe7a712
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2024-10-13 10:07:19 +0700

    Functions dengan Pengembalian dan Fungsi main

--- app/src/main/java/com/example/muzaka1project/Functions.kt
+++ app/src/main/java/com/example/muzaka1project/Functions.kt
@@ -20,11 +20,24 @@ class Functions {
         println("Hello $name! You are $age years old.")
     }
 
-    //default parameter valeu
-    //default parameter valeu merupakan cara untuk memanggil default pada parameter fungsi
+    //default parameter value
+    //default parameter value merupakan cara untuk memanggil default pada parameter fungsi
     fun WithDefaultParameter(name: String = "Rafi", age: Int){
         println()
         println("== WithDefaultParameter ==")
         println("Hello $name! You are $age years old.")
+    }
+
+    fun WithReturn(panjang : Int, lebar : Int) : Int {
+        val luas = panjang * lebar
+        println("$luas")
+        return luas
+    }
+
+    fun main(){
+        WithoutParameter()
+        WithParameter("Uceng")
+        WithNamedArgument(name = "Uceng", age = 30)
+        WithDefaultParameter(age = 25)
     }
 }
